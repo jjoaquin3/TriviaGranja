f4d51ec0f49dd9470ecebd85b083f34b0fe66919
Joaquin <jjcacao2@gmail.com>
2016-09-04 03:04:56 -0600

    * Diseño pantalla 1, 3: terminado.
    * Pantalla 1, 4: Imagenes en multiples resoluciones, para adaptacion, "plugin Drawable Importer".
    
    * Pantalla 3: Animación boton regreso.

--- app/src/main/java/com/example/joaquin/triviagranja/MainActivity.java
+++ app/src/main/java/com/example/joaquin/triviagranja/MainActivity.java
@@ -4,6 +4,7 @@ import com.example.joaquin.triviagranja.jose.conteo;
 
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
     public void main_pantalla2(View v)

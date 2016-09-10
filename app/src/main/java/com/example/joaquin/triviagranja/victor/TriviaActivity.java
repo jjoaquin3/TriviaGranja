c4aef45bced2f2d4f2badb27377f064c94192f4e
victor <victorr.1827@gmail.com>
2016-09-09 19:52:10 -0500

    Mensaje de error Trivia 2

--- app/src/main/java/com/example/joaquin/triviagranja/victor/TriviaActivity.java
+++ app/src/main/java/com/example/joaquin/triviagranja/victor/TriviaActivity.java
@@ -218,7 +218,15 @@ public class TriviaActivity extends AppCompatActivity {
                     eltime = null;
                     finAct();
                 }
-            }else { flag = false; touch_active = false; System.out.println("una categoria null");}
+            }else { flag = false; touch_active = false; System.out.println("una categoria null");
+
+                Toast.makeText(this, "la categoria no es valida",Toast.LENGTH_LONG);
+                if(eltime!= null)
+                    eltime.cancel();
+                eltime = null;
+                finAct();
+
+            }
         }
     }
 
@@ -259,9 +267,25 @@ public class TriviaActivity extends AppCompatActivity {
                         if (posTmp >= (limitecorrectas + limiteincorrectas)) posTmp = 0;
                         aux++;
                     }
-                } else { flag = false; touch_active = false; System.out.println("No hay suficientes respuestas"); }
-            } else { flag = false; touch_active = false; System.out.println("Respuestas es null"); }
-        }else { flag = false; touch_active = false; System.out.println("Pregunta es null"); }
+                } else { flag = false; touch_active = false; System.out.println("No hay suficientes respuestas");
+                    Toast.makeText(this, "No hay suficientes respuestas  ",Toast.LENGTH_LONG);
+                    if(eltime!= null)
+                        eltime.cancel();
+                    eltime = null;
+                    finAct();
+                }
+            } else { flag = false; touch_active = false; System.out.println("Respuestas es null");
+                Toast.makeText(this, "Respuestas es null  ",Toast.LENGTH_LONG);
+                if(eltime!= null)
+                    eltime.cancel();
+                eltime = null;
+                finAct();}
+        }else { flag = false; touch_active = false; System.out.println("Pregunta es null");
+            Toast.makeText(this, "Pregunta es null ",Toast.LENGTH_LONG);
+            if(eltime!= null)
+                eltime.cancel();
+            eltime = null;
+            finAct();}
     }
 
     private void iniciar()
